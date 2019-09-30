4ab65e02c34513cc7669691b776904f97fe3fea2
Alexandre ROUSSEL <alexandre.roussel@insa-lyon.fr>
2019-09-30 20:45:24 +0200

    Facebook and Planete cohabitation + login
    Unicity of variables and planete login + marks' displayed

--- index.js
+++ index.js
@@ -1,35 +1,53 @@
 require('dotenv').config();
 const puppeteer = require('puppeteer');
 
-// Fill in .env file
 const FACEBOOK_EMAIL = process.env.FACEBOOK_EMAIL;
 const FACEBOOK_PASSWORD = process.env.FACEBOOK_PASSWORD;
 
 const MESSENGER_URL = process.env.MESSENGER_URL;
+
+const PLANETE_USERNAME = process.env.PLANETE_USERNAME;
+const PLANETE_PASSWORD = process.env.PLANETE_PASSWORD;
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     page.setViewport({ width: 1280, height: 720 });
 
+    // PLANETE
+    await page.goto('https://login.insa-lyon.fr/cas/login', { waitUntil: 'networkidle2' });
+    let usernamePlaneteField = await page.$('[name=username]');
+    let passwordPlaneteField = await page.$('[name=password]');
+    let submitPlaneteButton = await page.$('[name=submit]');
+
+    await usernamePlaneteField.type(PLANETE_USERNAME);
+    await passwordPlaneteField.type(PLANETE_PASSWORD);
+
+    // Wait til submit button is clicked and the page is loaded
+    const navigationPlanetePromise = page.waitForNavigation();
+
+    await submitPlaneteButton.click();
+    
+    // End Wait
+    await navigationPlanetePromise;
+
+    await page.goto('https://planete.insa-lyon.fr/uPortal/f/for/normal/render.uP?pCt=scolarix-portlet.u18l1n13&pP_action=notes', { waitUntil: 'networkidle2' });
+
     // MESSENGER
     await page.goto('https://www.messenger.com/', { waitUntil: 'networkidle2' });
 
-    let emailField = await page.$('[name=email]');
-    let passwordField = await page.$('[name=pass]');
-    let submitButton = await page.$('button');
+    let emailFacebookField = await page.$('[name=email]');
+    let passwordFacebookField = await page.$('[name=pass]');
+    let submitFacebookButton = await page.$('button');
 
-    await emailField.type(FACEBOOK_EMAIL);
-    await passwordField.type(FACEBOOK_PASSWORD);
-
+    await emailFacebookField.type(FACEBOOK_EMAIL);
+    await passwordFacebookField.type(FACEBOOK_PASSWORD);
     
-    // Wait til submit button is clicked and the page is loaded
-    const navigationPromise = page.waitForNavigation();
+    const navigationFacebookPromise = page.waitForNavigation();
 
-    await submitButton.click();
+    await submitFacebookButton.click();
 
-    // End Wait
-    await navigationPromise;
+    await navigationFacebookPromise;
 
     await page.goto(MESSENGER_URL, { waitUntil: 'networkidle2' });
 
