65cccfb29aedbf1fa88514cae6aaaf715c9d945e
Alexandre ROUSSEL <alexandre.roussel@insa-lyon.fr>
2019-11-18 02:24:15 +0100

    Handle promises + fix messenger login button
    - Unhandled promises are deprecated, then addition of big try catch block... So ugly ;(
    - Focus select on messenger login button

--- index.js
+++ index.js
@@ -12,105 +12,109 @@ const PLANETE_USERNAME = process.env.PLANETE_USERNAME;
 const PLANETE_PASSWORD = process.env.PLANETE_PASSWORD;
 
 (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    page.setViewport({ width: 1280, height: 720 });
-
-    // PLANETE
-    await page.goto('https://login.insa-lyon.fr/cas/login', { waitUntil: 'networkidle2' });
-    let usernamePlaneteField = await page.$('[name=username]');
-    let passwordPlaneteField = await page.$('[name=password]');
-    let submitPlaneteButton = await page.$('[name=submit]');
-
-    await usernamePlaneteField.type(PLANETE_USERNAME);
-    await passwordPlaneteField.type(PLANETE_PASSWORD);
-
-    // Wait til submit button is clicked and the page is loaded
-    const navigationPlanetePromise = page.waitForNavigation();
-
-    await submitPlaneteButton.click();
-    
-    // End Wait
-    await navigationPlanetePromise;
-
-    await page.goto('https://planete.insa-lyon.fr/uPortal/f/for/normal/render.uP?pCt=scolarix-portlet.u18l1n13&pP_action=notes', { waitUntil: 'networkidle2' });
-
-    const elts = await page.$$eval('.ec-exam', elts => elts.map(elt => elt.textContent.replace(/(\t)/gm, "")));
-    let results = elts.map(elt => elt.split('\n'));
-    let marks = [];
-    for(let i = 0; i < results.length; i++){
-        let name = results[i][1];
-        if(name == 'examen'){
-            continue;
-        }
-        let mark = results[i][5];
-        let average = results[i][6];
-        let sd = results[i][7];
-        marks.push({name: name, mark: mark, average: average, sd: sd});
-    }
-    
-    let fileContent = fs.readFileSync('./marks.txt', {flag: 'a+', encoding: 'utf8'});
-
-    // Initiliaze to 0 for firt start
-    let marks_parsed = 0;
     try{
-        marks_parsed = JSON.parse(fileContent);
-    } catch(e){
-        console.error('Parsing error', e);
-    }
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        page.setViewport({ width: 1280, height: 720 });
 
-    let missing_marks = [];
-    if(marks.length != marks_parsed.length){
-        for(let i = 0; i < marks.length; i++){
-            let test = false;
-            for(let j = 0; j < marks_parsed.length; j++){
-                if(marks[i].name == marks_parsed[j].name){
-                    test = true;
-                    break;
+        // PLANETE
+        await page.goto('https://login.insa-lyon.fr/cas/login', { waitUntil: 'networkidle2' });
+        let usernamePlaneteField = await page.$('[name=username]');
+        let passwordPlaneteField = await page.$('[name=password]');
+        let submitPlaneteButton = await page.$('[name=submit]');
+
+        await usernamePlaneteField.type(PLANETE_USERNAME);
+        await passwordPlaneteField.type(PLANETE_PASSWORD);
+
+        // Wait til submit button is clicked and the page is loaded
+        const navigationPlanetePromise = page.waitForNavigation();
+
+        await submitPlaneteButton.click();
+
+        // End Wait
+        await navigationPlanetePromise;
+
+        await page.goto('https://planete.insa-lyon.fr/uPortal/f/for/normal/render.uP?pCt=scolarix-portlet.u18l1n13&pP_action=notes', { waitUntil: 'networkidle2' });
+        const elts = await page.$$eval('.ec-exam', elts => elts.map(elt => elt.textContent.replace(/(\t)/gm, "")));
+        let results = elts.map(elt => elt.split('\n'));
+        let marks = [];
+        for(let i = 0; i < results.length; i++){
+            let name = results[i][1];
+            if(name == 'examen'){
+                continue;
+            }
+            let mark = results[i][5];
+            let average = results[i][6];
+            let sd = results[i][7];
+            marks.push({name: name, mark: mark, average: average, sd: sd});
+        }
+
+        let fileContent = fs.readFileSync('./marks.txt', {flag: 'a+', encoding: 'utf8'});
+        
+        // Initiliaze to 0 for firt start
+        let marks_parsed = 0;
+        try{
+            marks_parsed = JSON.parse(fileContent);
+        } catch(e){
+            console.error('Parsing error', e);
+        }
+
+        let missing_marks = [];
+        if(marks.length != marks_parsed.length){
+            for(let i = 0; i < marks.length; i++){
+                let test = false;
+                for(let j = 0; j < marks_parsed.length; j++){
+                    if(marks[i].name == marks_parsed[j].name){
+                        test = true;
+                        break;
+                    }
+                }
+                if(test == false){
+                    missing_marks.push({name: marks[i].name, mark: marks[i].mark, average: marks[i].average, sd: marks[i].sd});
                 }
             }
-            if(test == false){
-                missing_marks.push({name: marks[i].name, mark: marks[i].mark, average: marks[i].average, sd: marks[i].sd});
-            }
+            fs.writeFileSync('./marks.txt', JSON.stringify(marks, {flag : 'w', encoding : 'utf8'}));
         }
-        fs.writeFileSync('./marks.txt', JSON.stringify(marks, {flag : 'w', encoding : 'utf8'}));
-    }
 
-    if(missing_marks.length != 0){
-        for(let i = 0; i < missing_marks.length; i++){
-            console.log('Nouvelle note ajoutée : ' + missing_marks[i].name +
+        if(missing_marks.length != 0){
+            for(let i = 0; i < missing_marks.length; i++){
+                console.log('Nouvelle note ajoutée : ' + missing_marks[i].name +
                 ' avec une moyenne de : ' + missing_marks[i].average +
                 ' et un écart-type de : ' + missing_marks[i].sd);
+            }
         }
+        else{
+            await browser.close()
+        }
+
+        // MESSENGER
+        await page.goto('https://www.messenger.com/', { waitUntil: 'networkidle2' });
+
+        let emailFacebookField = await page.$('[name=email]');
+        let passwordFacebookField = await page.$('[name=pass]');
+        let submitFacebookButton = await page.$('button[id=loginbutton]');
+        
+        await emailFacebookField.type(FACEBOOK_EMAIL);
+        await passwordFacebookField.type(FACEBOOK_PASSWORD);
+        
+        const navigationFacebookPromise = page.waitForNavigation();
+
+        await submitFacebookButton.click();
+
+        await navigationFacebookPromise;
+
+        await page.goto(MESSENGER_URL, { waitUntil: 'networkidle2' });
+
+        for(let i = 0; i < missing_marks.length; i++){
+            await page.type('._1p1t', 'Nouvelle note ajoutée : ' + missing_marks[i].name +
+            ' avec une moyenne de : ' + missing_marks[i].average +
+            ' et un écart-type de : ' + missing_marks[i].sd);
+            await page.keyboard.press('Enter');
+        }
+        await browser.close();
     }
-    else{
-        await browser.close()
+    catch (err) {
+        console.error(err.message);
     }
-    
-    // MESSENGER
-    await page.goto('https://www.messenger.com/', { waitUntil: 'networkidle2' });
-
-    let emailFacebookField = await page.$('[name=email]');
-    let passwordFacebookField = await page.$('[name=pass]');
-    let submitFacebookButton = await page.$('button');
-
-    await emailFacebookField.type(FACEBOOK_EMAIL);
-    await passwordFacebookField.type(FACEBOOK_PASSWORD);
-    
-    const navigationFacebookPromise = page.waitForNavigation();
-
-    await submitFacebookButton.click();
-
-    await navigationFacebookPromise;
-
-    await page.goto(MESSENGER_URL, { waitUntil: 'networkidle2' });
-
-    for(let i = 0; i < missing_marks.length; i++){
-        await page.type('._1p1t', 'Nouvelle note ajoutée : ' + missing_marks[i].name +
-        ' avec une moyenne de : ' + missing_marks[i].average +
-        ' et un écart-type de : ' + missing_marks[i].sd);
-        await page.keyboard.press('Enter');
-    }
-    await browser.close()
 })();
 
